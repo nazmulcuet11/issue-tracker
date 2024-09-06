1d6f31495022c942b6f8431970794cf15d2e336f
Nazmul Islam <nazmul.islam@agoda.com>
2024-09-06 18:03:58 +0700

    rename contentNegotiation to configureSerialization for consistent naming

--- src/main/kotlin/com/issueTracker/plugins/Serialization.kt
+++ src/main/kotlin/com/issueTracker/plugins/Serialization.kt
@@ -5,7 +5,7 @@ import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import kotlinx.serialization.json.Json
 
-fun Application.contentNegotiation() {
+fun Application.configureSerialization() {
     install(ContentNegotiation) {
         json(Json {
             prettyPrint = true
