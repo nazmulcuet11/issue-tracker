4fde7c0b52fb51cd7c835b0a911dac500f17240d
nazmulcuet11 <nazmulcuet11@gmail.com>
2024-09-08 17:36:14 +0700

    add db dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
-@Suppress("VariableNaming")
+@file:Suppress("PropertyName")
 val kotlin_version: String by project
-@Suppress("VariableNaming")
 val logback_version: String by project
-@Suppress("VariableNaming")
 val ktor_version: String by project
-@Suppress("VariableNaming")
 val koin_version: String by project
+val exposed_version: String by project
+val postgres_version: String by project
+val hikari_cp_version: String by project
 
 plugins {
     kotlin("jvm") version "2.0.0"
@@ -46,6 +46,14 @@ dependencies {
     implementation("io.insert-koin:koin-ktor:$koin_version")
     // SLF4J Logger for Koin
     implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
+
+    // DB
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
+    implementation("org.postgresql:postgresql:$postgres_version")
+    implementation("com.zaxxer:HikariCP:$hikari_cp_version")
 
     // test
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
